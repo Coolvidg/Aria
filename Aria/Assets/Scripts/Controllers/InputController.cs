eadb19b71dcc33599c3636d8ab0976ba4acf5b76
Coolpic783 <58189796+coolpic783@users.noreply.github.com>
2024-01-28 20:13:42 -0600

    some progress on character controller

--- Aria/Assets/Scripts/Controllers/InputController.cs
+++ Aria/Assets/Scripts/Controllers/InputController.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputController : MonoBehaviour
+public abstract class InputController : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract float RetrieveMoveInput();
+    public abstract bool RetrieveJumpInput();
 }
