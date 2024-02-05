c01eb90ae87b1cac3a4a750fd00f4ae6752a0a36
Alice Randles <58189796+coolpic783@users.noreply.github.com>
2024-02-05 13:01:05 -0600

    Update UIManager.cs

--- Aria/Assets/Scripts/UIManager.cs
+++ Aria/Assets/Scripts/UIManager.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System Collections;
+using VIDE_Data;
+using UnityEngine.UI
 
-public class UIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class UIManager : MonoBehaviour {
+
+    public GameObject container_NPC;
+    public GameObject container_PLAYER;
+    public Text text_NPC;
+    public Text[] text_Choices;
+
+    void Start () {
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!VD.isActive)
+            {
+                Begin();
+            }
+        }
+    }
+
+    void Begin()
     {
-        
+        VD.OnNodeChange += UpdateUI;
+        VD.OnEnd += End;
+        VD.BeginDialogue(GetComponent<VIDE_Assign>());
+    }
+
+    void UpdateUI(VD.NodeData data)
+    {
+    
+    }
+
+    void End(VD.NodeData data)
+    {
+        VD.OnNodeChange -= UpdateUI;
+        VD.OnEnd -= End;
+        VD.EndDialogue();
+    }
+
+    void OnDisable()
+    {
+        if (container_NPC != null)
+            End(null);
     }
 }
