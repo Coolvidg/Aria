838e5c716e2fce78dbfe72b964ca533829158944
Coolpic783 <58189796+coolpic783@users.noreply.github.com>
2024-02-16 20:29:01 -0600

    Dialogue system test complete!

--- Aria/Assets/Scripts/UIManager.cs
+++ Aria/Assets/Scripts/UIManager.cs
@@ -10,8 +10,11 @@ public class UIManager : MonoBehaviour {
     public Text text_NPC;
     public Text[] text_Choices;
 
-    void Start () {
-    
+    void Start ()
+    {
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
+
     }
 
     void Update () {
@@ -20,6 +23,10 @@ public class UIManager : MonoBehaviour {
             if (!VD.isActive)
             {
                 Begin();
+            }
+            else
+            {
+                VD.Next();
             }
         }
     }
@@ -33,11 +40,36 @@ public class UIManager : MonoBehaviour {
 
     void UpdateUI(VD.NodeData data)
     {
-    
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
+
+        if (data.isPlayer)
+        {
+            container_PLAYER.SetActive(true);
+
+            for (int i = 0; i < text_Choices.Length; i++)
+            {
+                if (i < data.comments.Length)
+                {
+                    text_Choices[i].transform.parent.gameObject.SetActive(true);
+                    text_Choices[i].text = data.comments[i];
+                } else
+                {
+                    text_Choices[i].transform.parent.gameObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            container_NPC.SetActive(true);
+            text_NPC.text = data.comments[data.commentIndex];
+        }
     }
 
     void End(VD.NodeData data)
     {
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
         VD.OnNodeChange -= UpdateUI;
         VD.OnEnd -= End;
         VD.EndDialogue();
@@ -47,5 +79,12 @@ public class UIManager : MonoBehaviour {
     {
         if (container_NPC != null)
             End(null);
+    }
+
+    public void SetPlayerChoice(int choice)
+    {
+        VD.nodeData.commentIndex = choice;
+        if (Input.GetMouseButtonUp(0))
+            VD.Next();
     }
 }
