fbe5d15c8e7913c22ab0ad27d05a62a9b84d847c
Alice Randles <58189796+coolpic783@users.noreply.github.com>
2024-02-17 15:34:13 -0600

    Update Ground.cs

--- Aria/Assets/Scripts/Checks/Ground.cs
+++ Aria/Assets/Scripts/Checks/Ground.cs
@@ -3,54 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ground : MonoBehaviour
-{
-    private bool onGround;
-	private float friction;
-	
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-		EvaluateCollision(collision);
-		RetrieveFriction(collision);
-	}
-	
-	private void OnCollisionStay2D(Collision2D collision)
-	{
-		EvaluateCollision(collision);
-		RetrieveFriction(collision);
-	}
-	
-	private void OnCollisionExit2D(Collision2D collision)
-	{
-		onGround = false;
-		friction = 0;
-	}
-	private void EvaluateCollision(Collision2D collision)
-	{
-		for(int i=0; i < collision.contactCount; i++)
-		{
-			Vector2 normal = collision.GetContact(i).normal;
-			onGround = normal.y >= 0.9f;
-		}
-	}
-	
-	private void RetrieveFriction(Collision2D collision)
-	{
-		PhysicsMaterial2D material = collision.rigidbody.sharedMaterial;
-		
-		friction = 0;
-		
-		if (material != null)
-		{
-			friction = material.friction;
-		}
-	}
-	
-	public bool GetOnGround()
-	{
-		return onGround;
-	}
-	public float GetFriction()
-	{
-		return friction;
-	}
-}
+    {
+        public bool OnGround { get; private set; }
+        public float Friction { get; private set; }
+
+        private Vector2 _normal;
+        private PhysicsMaterial2D _material;
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            OnGround = false;
+            Friction = 0;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            EvaluateCollision(collision);
+            RetrieveFriction(collision);
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            EvaluateCollision(collision);
+            RetrieveFriction(collision);
+        }
+
+        private void EvaluateCollision(Collision2D collision)
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                _normal = collision.GetContact(i).normal;
+                OnGround |= _normal.y >= 0.9f;
+            }
+        }
+
+        private void RetrieveFriction(Collision2D collision)
+        {
+            _material = collision.rigidbody.sharedMaterial;
+
+            Friction = 0;
+
+            if(_material != null)
+            {
+                Friction = _material.friction;
+            }
+        }
+    }
