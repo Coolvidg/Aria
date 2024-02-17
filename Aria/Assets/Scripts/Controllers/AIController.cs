38206a9177de69ab3f0f529b290b3d0d61f39dc5
Alice Randles <58189796+coolpic783@users.noreply.github.com>
2024-02-17 15:36:10 -0600

    Update AIController.cs

--- Aria/Assets/Scripts/Controllers/AIController.cs
+++ Aria/Assets/Scripts/Controllers/AIController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "AIContoller", menuName = "InputController/AIContoller")]
-public class AIContoller : InputController
-{
-    public override bool RetrieveJumpInput()
-	{
-		return true;
-	}
-	
-	public override float RetrieveMoveInput()
-	{
-		return 1f;
-	}
-}
+public class AIController : InputController
+    {
+        public override bool RetrieveJumpInput()
+        {
+            return true;
+        }
+
+        public override float RetrieveMoveInput()
+        {
+            return 1f;
+        }
+
+        public override bool RetrieveJumpHoldInput()
+        {
+            return false;
+        }
+    }
