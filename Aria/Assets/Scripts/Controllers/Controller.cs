abc17734859109d62bf2aa0ef6f16bafdd65772e
Alice Randles <58189796+coolpic783@users.noreply.github.com>
2024-02-17 15:38:20 -0600

    Update Controller.cs

--- Aria/Assets/Scripts/Controllers/Controller.cs
+++ Aria/Assets/Scripts/Controllers/Controller.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-    public abstract class InputController : ScriptableObject
+    public class Controller : MonoBehaviour
     {
-        public abstract float RetrieveMoveInput();
-        public abstract bool RetrieveJumpInput();
-        public abstract bool RetrieveJumpHoldInput();
+        public InputController input = null;
     }
