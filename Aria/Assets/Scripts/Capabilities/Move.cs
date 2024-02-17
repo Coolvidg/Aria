34999a7e725d9bc92aaf821481e162d75deb5e77
Alice Randles <58189796+coolpic783@users.noreply.github.com>
2024-02-17 15:33:00 -0600

    Update Move.cs

--- Aria/Assets/Scripts/Capabilities/Move.cs
+++ Aria/Assets/Scripts/Capabilities/Move.cs
@@ -2,44 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
-{
-    [SerializeField] private InputController input = null;
-	[SerializeField, Range(0f, 100f)] private float maxSpeed = 4f;
-	[SerializeField, Range(0f, 100f)] private float maxAcceleration = 35f;
-	[SerializeField, Range(0f, 100f)] private float maxAirAcceleration = 20f;
-	
-	private Vector2 direction;
-	private Vector2 desiredVelocity;
-	private Vector2 velocity;
-	private Rigidbody2D body;
-	private Ground ground;
-	
-	private float maxSpeedChange;
-	private float acceleration;
-	private bool onGround;
-	
-	void Awake()
-	{
-		body = GetComponent<Rigidbody2D>();
-		ground = GetComponent<Ground>();
-	}
-	
-	void Update()
-	{
-		direction.x = input.RetrieveMoveInput();
-		desiredVelocity = new Vector2(direction.x,0f) * Mathf.Max(maxSpeed - ground.GetFriction(),0f);
-	}
-	
-	private void FixedUpdate()
-	{
-		onGround = ground.GetOnGround();
-		velocity = body.velocity;
-		
-		acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-		maxSpeedChange = acceleration * Time.deltaTime;
-		velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-		
-		body.velocity = velocity;
-	}
-}
+[RequireComponent(typeof(Controller))]
+    public class Move : MonoBehaviour
+    {
+        [SerializeField, Range(0f, 100f)] private float _maxSpeed = 4f;
+        [SerializeField, Range(0f, 100f)] private float _maxAcceleration = 35f;
+        [SerializeField, Range(0f, 100f)] private float _maxAirAcceleration = 20f;
+
+        private Controller _controller;
+        private Vector2 _direction, _desiredVelocity, _velocity;
+        private Rigidbody2D _body;
+        private Ground _ground;
+
+        private float _maxSpeedChange, _acceleration;
+        private bool _onGround;
+
+        private void Awake()
+        {
+            _body = GetComponent<Rigidbody2D>();
+            _ground = GetComponent<Ground>();
+            _controller = GetComponent<Controller>();
+        }
+
+        private void Update()
+        {
+            _direction.x = _controller.input.RetrieveMoveInput();
+            _desiredVelocity = new Vector2(_direction.x, 0f) * Mathf.Max(_maxSpeed - _ground.Friction, 0f);
+        }
+
+        private void FixedUpdate()
+        {
+            _onGround = _ground.OnGround;
+            _velocity = _body.velocity;
+
+            _acceleration = _onGround ? _maxAcceleration : _maxAirAcceleration;
+            _maxSpeedChange = _acceleration * Time.deltaTime;
+            _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, _maxSpeedChange);
+
+            _body.velocity = _velocity;
+        }
+    }
